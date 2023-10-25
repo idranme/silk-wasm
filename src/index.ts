e831401891e7a7883f61d779afa46f5d4d960ea8
idanran <idanran@outlook.com>
2023-10-26 00:27:53 +0800

    1.5.0

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ export async function encode(input: Buffer, sampleRate: number): Promise<Buffer>
     const instance = await silkWasm()
     const u8Array = Uint8Array.from(input)
 
-    const arr: Uint8Array[] = []
+    const arr: Buffer[] = []
 
     const ret = instance.silk_encode(u8Array, u8Array.length, sampleRate, (chunk: Uint8Array) => {
-        arr.push(chunk)
+        arr.push(Buffer.from(chunk))
     })
 
     if (ret === 0) throw new Error('编码失败')
@@ -19,10 +19,10 @@ export async function decode(input: Buffer, sampleRate: number): Promise<Buffer>
     const instance = await silkWasm()
     const u8Array = Uint8Array.from(input)
 
-    const arr: Uint8Array[] = []
+    const arr: Buffer[] = []
 
     const ret = instance.silk_decode(u8Array, u8Array.length, sampleRate, (chunk: Uint8Array) => {
-        arr.push(chunk)
+        arr.push(Buffer.from(chunk))
     })
 
     if (ret === 0) throw new Error('解码失败')
