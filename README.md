4a1dbd6f2155d5ca2a0196ccfea36a6765c8b84c
idanran <idanran@outlook.com>
2023-10-26 20:22:59 +0800

    2.0.1

--- README.md
+++ README.md
@@ -7,13 +7,13 @@ QQ/微信语音编解码
 ## API
 ```ts
 // pcm 转 silk。input 为 pcm 文件，samplingRate 为采样率。 
-function encode(input: Buffer, sampleRate: number): Promise<Buffer>
+function encode(input: Uint8Array, sampleRate: number): Promise<Uint8Array>
 
 // silk 转 pcm。input 为 silk 文件，samplingRate 为采样率。 
-function decode(input: Buffer, sampleRate: number): Promise<Buffer>
+function decode(input: Uint8Array, sampleRate: number): Promise<Uint8Array>
 
 // 获取 silk 音频时长，输出单位为毫秒。
-function getDuration(silk: Buffer, frameMs?: number): number
+function getDuration(silk: Uint8Array, frameMs?: number): number
 ```
 
 ## Example
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,45 @@
 import silkWasm from './silk_wasm.js'
+import { concat } from './utils'
 
-export async function encode(input: Buffer, sampleRate: number): Promise<Buffer> {
+export async function encode(input: Uint8Array, sampleRate: number): Promise<Uint8Array> {
     const instance = await silkWasm()
-    const u8Array = Uint8Array.from(input)
 
-    const arr: Buffer[] = []
+    const arr: Uint8Array[] = []
+    let totalLength = 0
 
-    const ret = instance.silk_encode(u8Array, u8Array.length, sampleRate, (chunk: Uint8Array) => {
-        arr.push(Buffer.from(chunk))
+    const ret = instance.silk_encode(input, input.length, sampleRate, (chunk: Uint8Array) => {
+        totalLength += chunk.length
+        arr.push(Uint8Array.from(chunk))
     })
 
-    if (ret === 0) throw new Error('编码失败')
+    if (ret === 0) throw new Error('silk encoding failure')
 
-    return Buffer.concat(arr).subarray(0, -1)
+    return concat(arr, totalLength).subarray(0, -1)
 }
 
-export async function decode(input: Buffer, sampleRate: number): Promise<Buffer> {
+export async function decode(input: Uint8Array, sampleRate: number): Promise<Uint8Array> {
     const instance = await silkWasm()
-    const u8Array = Uint8Array.from(input)
 
-    const arr: Buffer[] = []
+    const arr: Uint8Array[] = []
+    let totalLength = 0
 
-    const ret = instance.silk_decode(u8Array, u8Array.length, sampleRate, (chunk: Uint8Array) => {
-        arr.push(Buffer.from(chunk))
+    const ret = instance.silk_decode(input, input.length, sampleRate, (chunk: Uint8Array) => {
+        totalLength += chunk.length
+        arr.push(Uint8Array.from(chunk))
     })
 
-    if (ret === 0) throw new Error('解码失败')
+    if (ret === 0) throw new Error('silk decoding failure')
 
-    return Buffer.concat(arr)
+    return concat(arr, totalLength)
 }
 
-export function getDuration(silk: Buffer, frameMs = 20): number {
+export function getDuration(silk: Uint8Array, frameMs = 20): number {
     const tencent = silk[0] === 0x02
     let offset = tencent ? 10 : 9
     let i = 0
+    const dataView = new DataView(silk.buffer)
     while (offset < silk.length) {
-        const size = silk.readUInt16LE(offset)
+        const size = dataView.getUint16(offset, true)
         offset += 2
         i += 1
         offset += size
